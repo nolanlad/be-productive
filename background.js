3ed317aedcb04e089bfd60f03e10166ebfed5759
nolanlad <nolanrmccarthy@gmail.com>
2021-03-02 20:55:26 -0500

    cleanup

--- background.js
+++ background.js
@@ -1,28 +1,3 @@
-
-// function listener(details) {
-//     let filter = browser.webRequest.filterResponseData(details.requestId);
-//     let decoder = new TextDecoder("utf-8");
-//     let encoder = new TextEncoder();
-  
-//     filter.ondata = event => {
-//       let str = decoder.decode(event.data, {stream: true});
-//       // Just change any instance of Example in the HTTP response
-//       // to WebExtension Example.
-//       document.body.style.border = "5px solid red";
-//       str = str.replace(/Example/g, 'WebExtension Example');
-//       filter.write(encoder.encode(str));
-//       filter.disconnect();
-//     }
-  
-//     return {};
-//   }
-  
-//   browser.webRequest.onBeforeRequest.addListener(
-//     listener,
-//     {urls: ["https://example.com/*"], types: ["main_frame"]},
-//     ["blocking"]
-//   );
-
 var pattern = ["*://*.reddit.com/*","*://*.nytimes.com/*","*://*.vox.com/*",
 "*://*.youtube.com/*"];
 
